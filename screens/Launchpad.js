38e629d4f6161e4cc80a9eb63599516afbde52f8
Gerben Mol <s1129477@student.hsleiden.nl>
2023-05-08 11:50:29 +0200

    Foto van launchpad word weergegeven als je er op een klikt

--- screens/Launchpad.js
+++ screens/Launchpad.js
@@ -1,15 +1,28 @@
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React, {useEffect, useState} from 'react';
+import {Button, Image, StyleSheet, Text, View} from 'react-native';
 import * as api from '../modules/api.js';
 
 export default function LaunchpadScreen({navigation, route}) {
     const {launchPadID, otherParam} = route.params;
-    (async () => {
-        let response = await api.createRequest("launchpads", launchPadID)
-        console.log(response)
-    })()
+
+    let [response, setResponse] = useState({
+        images: {
+            large: [
+                "https://google.com"
+            ]
+        }
+    });
+
+    useEffect(() => {
+        (async () => {
+            let result = await api.createRequest("launchpads", launchPadID)
+            setResponse(result)
+        })()
+    },[]);
+
     return (
         <View style={styles.container}>
+            <Image source={{uri: response.images.large[0]}} style={{width: '80%', height: '40%', borderRadius: 10}} resizeMode="contain"/>
             <Text>SettingsScreen2</Text>
             <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
             <Button title="Go back" onPress={() => navigation.goBack()} />
