fdbc32bc0668b19fab26a9038cf2cb36dd64279d
Max Overbeek <overbeekmax@gmail.com>
2023-05-09 20:37:24 +0200

    added basic styling to the settings page

--- screens/Settings.js
+++ screens/Settings.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TouchableOpacity, Image, SafeAreaView, ScrollView } from 'react-native';
 import * as api from '../modules/api';
+import arrow from '../imgs/back-black.png';
 
 export default function SettingsScreen({navigation}) {
 	return (
 	  <View style={styles.container}>
-		<Text>SettingsScreen</Text>
-		<Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
-		<Button title="Go back" onPress={() => navigation.goBack()} />
-		<Button title="Clear local stored data" onPress={() =>api.clearCache()} />
+			<TouchableOpacity style={{position: 'absolute', left: 0, top: 0, padding: 30, zIndex: 2}} onPress={() => navigation.goBack()}>
+				<Image style={{position: 'relative', top: 40}} source={arrow} />
+			</TouchableOpacity>
+			<Text style={{position: 'absolute', top: 70, fontWeight: 'bold', fontSize: 22}}>Settings</Text>
+			<View style={{backgroundColor: 'lightgrey', width: '100%', padding: 10, borderRadius: 10}}>
+				<Text style={{color: 'black', marginVertical: 5, fontWeight: 500, fontSize: 18}}>Cache</Text>
+				<Button title="Clear local stored data" onPress={() =>api.clearCache()} />
+			</View>
 	  </View>
 	);
 }
@@ -19,5 +24,6 @@ const styles = StyleSheet.create({
 		backgroundColor: '#fff',
 		alignItems: 'center',
 		justifyContent: 'center',
+		paddingHorizontal: 20,
 	},
 });
